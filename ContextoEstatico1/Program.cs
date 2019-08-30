03792c47229ebdb97613148a093721a139888a23
FatimaMillanes <fatima.rosela@hotmail.com>
2019-08-30 08:46:28 -0700

    Agregar archivos de proyecto.

---
+++ ContextoEstatico1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContextoEstatico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sumando1 = 10;
+            double sumando2 = 43;
+            double resultado =
+                Matematicas.sumar(sumando1, sumando2);
+
+            Console.WriteLine(sumando1.ToString() +
+                " + " + sumando2.ToString() +
+                " + " + resultado.ToString());
+
+            double radio = 3.67;
+            Console.WriteLine("PI: " + Matematicas.PI);
+
+
+            Console.WriteLine("Radio: "+ radio.ToString() + ", Area: " + Matematicas.areaCirculo(radio));
+
+
+
+            Console.Read();
+        }
+    }
+}
